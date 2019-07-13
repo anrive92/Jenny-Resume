0f235fd840c468b7714888010c14515b6b5d462e
anrive92 <angelriveravega@yahoo.com>
2019-07-13 10:06:01 -0500

    Add files via upload

---
+++ js/piechart/easypiechart.js
@@ -0,0 +1,11 @@
+$(function() {
+        $('.chart').easyPieChart({
+            size: 155,
+            barColor: "#5ce1e6",
+            scaleColor: false,
+            lineWidth: 8,
+            trackColor: "#fff",
+            lineCap: "butt",
+            scaleLength: 8,
+        });
+    });
---
+++ js/ui.js
@@ -0,0 +1,13 @@
+sal({
+  threshold: .5,
+  once: false,
+});
+
+var typed = new Typed(".type", {
+  strings: ["I'm passionate about management", "I enjoy building personnel", "I love traveling"],
+  typeSpeed: 50,
+  backSpeed: 40,
+  loop: true
+   // Default value
+});
+
